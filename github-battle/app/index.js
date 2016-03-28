a8df8a2b8515e9d37f021de62f4b554ab74bfef2
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-03-28 15:43:40 -0400

    nested components and Props

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -9,7 +9,41 @@ var HelloWorld = React.createClass({
   }
 });
 
+var HelloUser = React.createClass({
+  render: function(){
+    // var name = "Tyler Smith"
+    var name = this.props.name
+    var friends = ["Ean Platter", "Murthy Scott", "Merrys Cristian"]
+    return (
+      <div>
+        <h3>Name: {name} </h3>
+        <ShowList names = {friends} />
+      </div>
+    )
+  }
+});
+
+var ShowList = React.createClass({
+  render: function(){
+    var listItems = this.props.names.map(function(friend){
+      return <li> {friend} </li>;
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(
   <HelloWorld name="Aleksa"/>,
   document.getElementById('app')
 );
+
+ReactDOM.render(
+  <HelloUser name="Roxy"/>, document.getElementById("user")
+)
