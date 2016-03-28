df12c346503f2c79801bae40122609e415844e15
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-03-28 17:30:09 -0400

    adds link and github components

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,17 +1,77 @@
+// Focused
+// Independent
+// Reusable
+// Small
+// Testable
+
+//npm run start(to run webpack-dev-server)
+
+var USER_DATA = {
+  name: "Aleksa Matiyev",
+  username: "aleksa000777",
+  image: "http://aleksamatiyev.com/assets/img/bottom/logo.png"
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+
+
+var ProfilePic = React.createClass({
+  render: function(){
+    return <img src = {this.props.imageUrl} style={{height:100, width:100}} />
+  }
+})
+
+var ProfileLink = React.createClass({
   render: function(){
     return (
-      <div> Hello Reactjs Program, {this.props.name} </div>
+      <div>
+        <a href={"https://www.github.com/" + this.props.username} target="_blank">
+          {this.props.username}
+        </a>
+      </div>
     )
   }
 });
 
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <h2>{this.props.name} </h2>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <Avatar user={USER_DATA}/>,
+  document.getElementById('app')
+);
+
+
+
+
+
+
+
+///====================================//
+//===========example==================//
+
+
 var HelloUser = React.createClass({
   render: function(){
-    // var name = "Tyler Smith"
     var name = this.props.name
     var friends = ["Ean Platter", "Murthy Scott", "Merrys Cristian"]
     return (
@@ -39,11 +99,6 @@ var ShowList = React.createClass({
   }
 });
 
-ReactDOM.render(
-  <HelloWorld name="Aleksa"/>,
-  document.getElementById('app')
-);
-
-ReactDOM.render(
-  <HelloUser name="Roxy"/>, document.getElementById("user")
-)
+// ReactDOM.render(
+//   <HelloUser name="Roxy"/>, document.getElementById("user")
+// )
