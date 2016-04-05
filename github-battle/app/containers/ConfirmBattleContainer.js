98fbe2d867179b94fc59430f637b4dbb79cd4056
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-04-05 18:56:06 -0400

    result route

--- github-battle/app/containers/ConfirmBattleContainer.js
+++ github-battle/app/containers/ConfirmBattleContainer.js
@@ -10,23 +10,22 @@ var ConfirmBattleContainer = React.createClass({
     console.log("getInitialState");
     return {
       isLoading: true,
-      playerInfo: []
+      playersInfo: []
     }
   },
   componentWillMount: function(){
     console.log("componentWillMount");
   },
   componentDidMount: function(){
-    var temp = this;
     var query = this.props.location.query;
     console.log("componentDidMount");
     githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
       .then(function(players){
-        temp.setState({
+        this.setState({
           isLoading: false,
           playersInfo: [players[0], players[1]]
         })
-      })
+      }.bind(this))
   },
   componentWillReceiveProps: function(){
     console.log("componentWillReceiveProps");
@@ -34,11 +33,20 @@ var ConfirmBattleContainer = React.createClass({
   componentWillUnmount: function(){
     console.log("componentWillUnmount");
   },
+  handleInitiateBattle: function(){
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
 
   render: function(){
     return(
       <ConfirmBattle
         isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo}
 
       />
